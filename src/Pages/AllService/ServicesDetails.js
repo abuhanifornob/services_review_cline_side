65aa9087121fea57628929d97026aee76e0c5a59
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-11-10 18:56:33 +0600

    view full paes setup

--- src/Pages/AllService/ServicesDetails.js
+++ src/Pages/AllService/ServicesDetails.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const ServicesDetails = () => {
-    const servicesDetails=useLoaderData();
+    const servicesDetails = useLoaderData();
     console.log(servicesDetails);
     return (
-        <div>
-            <h1>This is Sevice Details{}</h1>
+        <div className="card w-100 bg-base-100 shadow-2xl">
+            <figure><img src={servicesDetails.img} alt="Shoes" /></figure>
+
+            <div className="card-body">
+                <h2 className="card-title">
+                    {servicesDetails.title}
+
+                </h2>
+                <p>
+                {servicesDetails.description}
+
+                </p>
+                <div className="card-actions justify-end">
+                    <Link to={`/comments/${servicesDetails._id}`}>
+                        <button className="btn btn-primary">Your Comments</button>
+                    </Link>
+                    <Link to={`/viewreview/${servicesDetails._id}`}>
+                        <div className="btn btn-primary">View Review</div>
+                    </Link>
+
+                </div>
+            </div>
         </div>
     );
 };
